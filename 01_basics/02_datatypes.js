7276e2c666fc2bb75110b4e2c912e690c41a01b6
keshavjha205 <156219148+keshavjha205@users.noreply.github.com>
2024-06-16 15:18:23 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) // gives errer; cuz we are using nodejs, not browser
+
+console.log(3 + 3);
+
+// number => 2 to the power 53
+let age = 20 
+
+// bigint => for big numbers
+let bignumber = 12345678910
+
+// String => always prefer in ""
+let name = "keshav"
+
+// Boolean => true/false
+let isLoggedIn = true
+
+// null => standalone value
+
+// undefined =>
+let userEmail;
+let userEmail2 = undefined
+
+// symbol => unique
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
