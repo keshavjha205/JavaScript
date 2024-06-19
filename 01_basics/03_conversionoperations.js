2420cf48a658ec21fc4b67711490f640b01f36ed
keshavjha205 <156219148+keshavjha205@users.noreply.github.com>
2024-06-19 04:28:23 +0000

    conversion and operations

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,70 @@
+// let score = undefined
+
+// console.log(typeof score);
+// console.log((typeof score));
+
+
+// let valueInNumber = Number (score) // converting into number
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// CONVERTS INTO
+// "33"(string) => 33
+// "33abc" => NaN
+//(boolean values) true => 1; false => 0
+// null => 0
+// undefined => NaN
+
+// let isLoggedIn = "keshav"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)// conversion me first letter hamesha capital hoga
+// console.log(booleanIsLoggedIn);
+
+// jab boolean me conversion krne pr 
+// yeh ---> converts into
+// 1    => true
+// 0    => false
+// ""(empty string) => false
+// "keshav" => true
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+
+
+// ***************** Operations *****************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(4/2);
+// console.log(2**2); // power ke liye do baar * lagana pdta hai...
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = " keshav"
+// let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); // agr phle string hoga toh piche walo me string me convert kar denge
+// console.log(1 + 2 + "3"); //agr normal hai toh phle operation perform karenge phir conversion karenge
+
+
+console.log(+true);
+console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
