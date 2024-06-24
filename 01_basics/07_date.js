b4aeb07bd6e6828551c5e89399abbf8f55f9bbf1
keshavjha205 <156219148+keshavjha205@users.noreply.github.com>
2024-06-24 13:29:45 +0000

    date

--- 01_basics/07_date.js
+++ 01_basics/07_date.js
@@ -15,4 +15,15 @@ let myDate = new Date()
 // let myCreatedDate =  new Date("2023-01-14") 
 // console.log(myCreatedDate.toDateString());
 
-let myTimestamp = Date.now()
+// let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+// console.log(newDate.getMonth() + 1);
+
+newDate.toLocaleString('default', {
+    weekday: "long"
+})
