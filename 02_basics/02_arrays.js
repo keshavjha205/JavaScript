eab5a6d713f3f034fb5d58469626428bfae096fb
keshavjha205 <156219148+keshavjha205@users.noreply.github.com>
2024-06-24 15:07:49 +0000

    arrays

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,8 @@
+const marvel_heros =["thor", "ironman", "captain"]
+const dc_heros = ["superman", "batman", "flash"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+
+const all_heros = marvel_heros.concat(dc_heros)
+console.log(all_heros);
