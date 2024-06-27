e159467fadbdd873ac104c619868581dfd327be0
keshavjha205 <156219148+keshavjha205@users.noreply.github.com>
2024-06-27 14:34:47 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,24 @@
+function addTwoNumbers(number1, number2){
+    console.log(number1 + number2);
+}
+
+function addTwoNumbers(number1, number2){
+    // let result = number1 + number2
+    // return result
+
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+function loginUserMessage(username = "sam"){
+    if(username === undefined){
+        console.log("Please enter a username");
+        return
+    }
+    return `${username}, just logged in`
+}
+// console.log(loginUserMessage("Keshav"))
+console.log(loginUserMessage()) 
