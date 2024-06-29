5168a53e921cb8b9ede45aae8bab0cfce0f33d29
keshavjha205 <156219148+keshavjha205@users.noreply.github.com>
2024-06-29 05:07:17 +0000

    function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,6 +1,7 @@
 function addTwoNumbers(number1, number2){
     console.log(number1 + number2);
 }
+addTwoNumbers(3,5)
 
 function addTwoNumbers(number1, number2){
     // let result = number1 + number2
@@ -21,4 +22,34 @@ function loginUserMessage(username = "sam"){
     return `${username}, just logged in`
 }
 // console.log(loginUserMessage("Keshav"))
-console.log(loginUserMessage()) 
+// console.log(loginUserMessage()) 
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(250, 300, 400));
+
+const user ={
+    userName : "prashant",
+    price: 49
+}
+
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.userName} and price is ${anyObject.price}`);
+} 
+
+// handleObject(user)
+handleObject({
+    userName: "rawat",
+    price : 99
+})
+
+const myArray = [200, 400, 600, 800]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myArray));
+console.log(returnSecondValue([400, 600, 800]));
