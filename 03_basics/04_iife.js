4c999fa41b7ee17b1dc4c08bdb6ef88dcc928e7c
keshavjha205 <156219148+keshavjha205@users.noreply.github.com>
2024-07-04 15:05:58 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,12 @@
+// Immedialtely Invoked Function Expression (IIFE)
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('keshav')
+
