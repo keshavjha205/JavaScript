de0fb3dffef0c3edd0de323bf95f9f0e40adab3f
keshavjha205 <156219148+keshavjha205@users.noreply.github.com>
2024-07-08 07:39:55 +0000

    conditionals

---
+++ 04_control_flow/01_conditionals.js
@@ -0,0 +1,70 @@
+// if-else
+
+const temp = 41
+
+// if(temp < 50){
+// console.log("less than 50");
+// }
+// else{
+//     console.log("temp is greater than 50");
+// }
+
+// some conditional opeartor
+// <, >, <=, >=, ==, !=, ===, !==
+
+const chakra = 200
+
+// if (chakra > 100){
+//     let power = "Rasengan"
+//     console.log(`user power: ${power}`);
+// }
+
+// const ki= 1000
+
+// if (ki > 500) console.log("galic gun"),console.log("kamehameha"); // ese code nahi likhna hai; aachi practice nahi hoti hai
+
+// domain, hollow purple, red, blue, limitless, black flash, 
+
+const cursedEnergy = 1000
+
+// if(cursedEnergy >= 10000){
+//     console.log("You can use Domain Expension");
+// }
+// else if(cursedEnergy >= 8000){
+//     console.log("You can use Hollow-purple");
+// }
+// else if(cursedEnergy >= 7000){
+//     console.log("You can use Red");
+// }
+// else if(cursedEnergy >= 6000){
+//     console.log("You can use Blue");
+// }
+// else if(cursedEnergy >= 5000){
+//     console.log("You can use Limitless");
+// }
+// else if(cursedEnergy >= 4000){
+//     console.log("You can use Black Flash");
+// }
+// else if(cursedEnergy >= 3000){
+//     console.log("You can use Teleportataion");
+// }
+// else if(cursedEnergy >= 2500){
+//     console.log("You can use Super-Strength");
+// }
+// else {
+//     console.log("You are mortal");
+// }
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if ( userLoggedIn && debitCard && 2==3)  {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
