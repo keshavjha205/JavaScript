e9297fa738b2995cdcdab3efb459a8245ace5ffc
keshavjha205 <156219148+keshavjha205@users.noreply.github.com>
2024-07-16 05:29:26 +0000

    while loop

---
+++ 05_iterations/02_while.js
@@ -0,0 +1,25 @@
+
+let index = 0;
+while (index <= 10) {
+    // console.log(`value of Index is ${index}`);
+    index = index +2;
+}
+
+let myArray = ["luffy", "zoro", "sanji"]
+
+let arr = 0;
+// while (arr < myArray.length) {
+//     // console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1;
+// }
+
+do {
+    console.log(`Character is ${myArray[arr]}`);
+    arr++;
+} while (arr < myArray.length);
+
+// let score = 1;
+// do {
+//     console.log(`Score is ${score}`);
+//     score++
+// } while (score <= 10);
