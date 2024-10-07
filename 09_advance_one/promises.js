d5523f84daa36a88bce57b73e69ba7cce532ea31
keshavjha205 <keshavjha1081@gmail.com>
2024-10-07 19:59:33 +0530

    promises

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -28,8 +28,45 @@ const promiseThree = new Promise(function(resolve, reject){
         resolve({userName: "codeJourney", email: "codejourney35@gmail.com"}) // generally object aata hai pr hum aur datatype bhi rakh skte hai
     },1000)
 })
-
 promiseThree.then(function(user){
     console.log(user);
 }) // jo bhi parameter resolve me daloge woh .then me execute hoga.
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = false; // true rakho toh error aaega false rakhoge toh nhi aaega
+        if (!error) {
+            resolve({userName:"keshav", password:"205"})
+        } else{
+            reject("ERROR: Something went wrong")
+        }
+    },1000)
+})
+promiseFour
+.then((user)=>{
+    console.log(user);
+    return user.userName;
+})
+.then((username)=>{
+    console.log(username);
+})
+.catch(function(error){
+    console.log(error);
+})
+.finally(()=> {
+    console.log("The promise is either resolve or rejected");
+    
+})
+
+
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = false; // true rakho toh error aaega false rakhoge toh nhi aaega
+        if (!error) {
+            resolve({userName:"javascript", password:"205"})
+        } else{
+            reject("ERROR: js went wrong")
+        }
+    },1000)
+})
 
